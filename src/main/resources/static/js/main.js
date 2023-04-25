3def75e14b89ebf9fd52574eada5ff279cfa0e54
David Gómez <dagove47@gmail.com>
2023-04-25 17:37:23 -0600

    Email.js added

--- src/main/resources/static/js/main.js
+++ src/main/resources/static/js/main.js
@@ -1,5 +1,45 @@
+
 const loader = document.getElementById("loader");
+const formNewMessage = document.getElementById('formNewMessage');
+const formInputs = document.querySelectorAll('#newName, #newEmail, #newSubject, #newMessage')
+const submitBtn = document.getElementById('submitBtn');
 
 window.addEventListener("load", () => {
     loader.style.display = "none";
 });
+
+/* 
+    Contact
+*/
+
+formNewMessage.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+
+    if (!formNewMessage.checkValidity()) {
+        formNewMessage.classList.add('was-validated');
+    } else {
+        submitBtn.value = 'Enviando...';
+        const serviceID = 'default_service';
+        const templateID = 'portfolio_form';
+
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                submitBtn.value = 'Enviar Mensaje!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert('Mensaje Enviado!');
+            }, (err) => {
+                submitBtn.value = 'Enviar Mensaje!';
+                clearInputs(formInputs);
+                formNewMessage.classList.remove('was-validated');
+                alert(JSON.stringify(err));
+            });
+    }
+});
+
+function clearInputs(inputs) {
+    inputs.forEach(input => {
+        input.value = '';
+    });
+}
